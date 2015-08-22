c7ca23f823853390df50a75579199f76e3a8e07c
Cale Hoopes <caledh@gmail.com>
2015-08-22 15:20:04 -0700

    Adding implementation and test file

--- util/Util.go
+++ util/Util.go
@@ -4,6 +4,8 @@ import (
 	"net/url"
 	"encoding/json"
 	"log"
+	"regexp"
+	"strconv"
 )
 
 func EncodeUrl(str string) (string, error) {
@@ -17,4 +19,43 @@ func EncodeUrl(str string) (string, error) {
 func PrintJson(x interface{}) {
 	y,_ := json.Marshal(&x)
 	log.Println(string(y))
+}
+
+func TimeToMillis(timeString string) int {
+
+	r, _ := regexp.Compile("(\\d+)(.*)")
+	matched := r.FindAllStringSubmatch(timeString,-1)
+	num, _ := strconv.Atoi(matched[0][1])
+	period := matched[0][2]
+	value := 0
+
+	switch period {
+	case "s":
+		value = num * 1000
+	case "m":
+		value = num * 1000*60
+	case "h":
+		value = num * 1000*60*60
+	case "d":
+		value = num * 1000*60*60*24
+	default:
+		value = num
+	}
+	return value
+}
+
+func CacheKeyToStatsD() {
+
+}
+
+func UrlToCacheKey() {
+
+}
+
+func UpdateTemplateVariables() {
+
+}
+
+func FilterCookies() {
+
 }
